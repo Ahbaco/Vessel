57abc8cdd3b7ff9066e2bce08b6d3b5796bb08c5
Vladimil Jimenez <vladimil.js@ahbaco.com>
2024-05-24 23:28:57 -0400

    fix: role guard

--- packages/common/src/decorators/authorized.decorator.ts
+++ packages/common/src/decorators/authorized.decorator.ts
@@ -1,19 +1,24 @@
 import { UseGuards, applyDecorators } from "@nestjs/common";
 import { ApiBearerAuth } from "@nestjs/swagger";
 import { Role } from "@vessel/database/enums";
-import { JwtAuthGuard } from "../guards";
+import { JwtAuthGuard, VerifiedGuard } from "../guards";
 import { RolesGuard } from "../guards/role.guard";
 import { Roles } from "./role.decorator";
 
 type AuthorizedOptions = {
   role?: Role;
+  verified?: boolean;
 };
 
-export const Authorized = ({ role }: AuthorizedOptions = {}) => {
+export const Authorized = ({ role, verified = true }: AuthorizedOptions = {}) => {
   const decorators = [ApiBearerAuth(), UseGuards(JwtAuthGuard)];
 
   if (role) {
     decorators.push(UseGuards(RolesGuard), Roles(role));
+  }
+
+  if (verified) {
+    decorators.push(UseGuards(VerifiedGuard));
   }
 
   return applyDecorators(...decorators);
--- packages/common/src/guards/role.guard.ts
+++ packages/common/src/guards/role.guard.ts
@@ -7,14 +7,21 @@ export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.getAllAndOverride<Role[]>("roles", [
+    const role = this.reflector.getAllAndOverride<Role>("role", [
       context.getHandler(),
       context.getClass(),
     ]);
-    if (!requiredRoles) {
+
+    if (!role) {
       return true;
     }
+
     const { user } = context.switchToHttp().getRequest();
-    return requiredRoles.some((role) => user.roles?.includes(role));
+
+    if (role === Role.Moderator && user.role === Role.Admin) {
+      return true;
+    }
+
+    return role === user.role;
   }
 }
